49ed7c9c6d14b6a5906349e118521bfeb7fcae19
Anton <antsht@gmail.com>
2024-06-12 17:57:24 +0300

    Implement Create, Remove, Eq, Sum and Sub functions

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -29,14 +29,43 @@ void s21_remove_matrix(matrix_t* A) {
   }
 }
 
+int s21_eq_matrix(matrix_t* A, matrix_t* B) {
+  int result = SUCCESS;
+  if (A->rows != B->rows || A->columns != B->columns) result = FAILURE;
+  for (int i = 0; i < A->rows && result == SUCCESS; i++) {
+    for (int j = 0; j < A->columns && result == SUCCESS; j++) {
+      if (fabs(A->matrix[i][j] - B->matrix[i][j]) > EPSILON) result = FAILURE;
+    }
+  }
+  return result;
+}
 
+int s21_s___matrix(matrix_t* A, matrix_t* B, int b_sign, matrix_t* result) {
+  int status = OK;
+  if (A->rows != B->rows || A->columns != B->columns) {
+    status = ERROR_INCORRECT_MATRIX;
+  }
+  if (status == OK) {
+    s21_remove_matrix(result);
+    status = s21_create_matrix(A->rows, A->columns, result);
+  }
+  if (status == OK) {
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < A->columns; j++) {
+        result->matrix[i][j] = A->matrix[i][j] + b_sign * B->matrix[i][j];
+      }
+    }
+  }
+
+  return status;
+}
+int s21_sum_matrix(matrix_t* A, matrix_t* B, matrix_t* result) {
+  return s21_s___matrix(A, B, 1, result);
+}
+int s21_sub_matrix(matrix_t* A, matrix_t* B, matrix_t* result) {
+  return s21_s___matrix(A, B, -1, result);
+}
 /*
-#define SUCCESS 1
-#define FAILURE 0
-int s21_eq_matrix(matrix_t* A, matrix_t* B);
-
-int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
-int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result);
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
@@ -49,3 +78,12 @@ int s21_determinant(matrix_t *A, double *result);
 
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
 */
+
+void s21_print_matrix(matrix_t* A) {
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->columns; j++) {
+      printf("%f ", A->matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -1,6 +1,8 @@
 #ifndef _MATRIX_H_
 #define _MATRIX_H_
 
+#include <math.h>
+#include <stdio.h>
 typedef struct matrix_struct {
   double** matrix;
   int rows;
@@ -19,8 +21,11 @@ void s21_remove_matrix(matrix_t* A);
 
 #define SUCCESS 1
 #define FAILURE 0
+#define EPSILON 1e-7L
+
 int s21_eq_matrix(matrix_t* A, matrix_t* B);
 
+int s21_s___matrix(matrix_t *A, matrix_t *B, int b_sign, matrix_t *result);
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
 
@@ -34,5 +39,7 @@ int s21_calc_complements(matrix_t *A, matrix_t *result);
 int s21_determinant(matrix_t *A, double *result);
 
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+
+void s21_print_matrix(matrix_t *A);
 
 #endif
